7a6f4ab2e61391a67ab049224adc7718bd5f05c5
eilinge <17601329166@163.com>
2024-06-17 21:23:13 +0800

    defind webui base url

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,7 +3,8 @@ import { dev } from '$app/environment';
 
 export const APP_NAME = 'Open WebUI';
 // export const WEBUI_BASE_URL = dev ? `http://${location.hostname}:8080` : ``;
-export const WEBUI_BASE_URL = dev ? `http://openui.gpt-oai.icu:8080/api/config` : ``;
+// export const WEBUI_BASE_URL = dev ? `http://openui.gpt-oai.icu:8080` : ``;
+export const WEBUI_BASE_URL = `http://openui.gpt-oai.icu:8080`;
 
 export const WEBUI_API_BASE_URL = `${WEBUI_BASE_URL}/api/v1`;
 
