54a3dc738fe9e7f2fdc3dad509df7257448c07de
wuchangqing <wuchangqing@chuangoaiot.com>
2024-06-17 15:47:17 +0800

    feat:cloud fire deploy

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -13,7 +13,7 @@ export const AUDIO_API_BASE_URL = `${WEBUI_BASE_URL}/audio/api/v1`;
 export const IMAGES_API_BASE_URL = `${WEBUI_BASE_URL}/images/api/v1`;
 export const RAG_API_BASE_URL = `${WEBUI_BASE_URL}/rag/api/v1`;
 
-export const WEBUI_VERSION = 'APP_VERSION';
+export const WEBUI_VERSION = '0.1.16';
 export const REQUIRED_OLLAMA_VERSION = '0.1.16';
 
 export const SUPPORTED_FILE_TYPE = [
